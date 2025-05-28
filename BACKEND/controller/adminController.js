e50e95c6b3b6f2db7acf5ff16b5e32b93a9281e7
Amey Kachare <113342195+amey-kachare@users.noreply.github.com>
2025-05-28 12:54:57 +0530

    hello

--- BACKEND/controller/adminController.js
+++ BACKEND/controller/adminController.js
@@ -1,4 +1,6 @@
 import bcrypt from "bcryptjs/dist/bcrypt";
+import User from "../models/User.js";
+import Tour from "../models/Tour.js";
 
 export const AdminLogin = async (req, res) => {
   try {
@@ -54,6 +56,51 @@ export const AdminLogin = async (req, res) => {
     
   } catch (err) {
      console.log(err);
-        res.status(500).json({ success: false, message: "Failed to login" });
+     return res.status(500).json({ success: false, message: "Failed to login" });
   }
 };
+
+export const getAllPendingTours = async (req, res)=>{
+    try{
+        const pendingTours = await Tour.find({isApproved : false, isRejected : false});
+
+        if(!pendingTours){
+            return res.status(404).json({success : false, message : "User not found"})
+        }
+        return res.status(200).json({success : true, message :"Tours fetched successfully", pendingTours})
+    }
+    catch(err){
+       return  res.status(500).json({ success: false, message: "Failed to Fetch" });
+    }
+}
+
+export const AcceptTour =  async (req , res)=>{
+    try {
+        const {id } = req.params;
+
+        const tour = await Tour.findById(id);
+        if(!tour){
+           return res.status(404).json({ success: false, message: "Tour not Found" });
+        }
+        tour.isApproved = true;
+        tour.save();
+       return  res.status(200).json({ success: true, message: "Tour Approved Successfully" });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Failed to Accept" });
+    }
+}
+
+export const RejectTour =  async (req , res)=>{
+    try {
+        const {id } = req.params;
+        const tour = await Tour.findById(id);
+        if(!tour){
+           return res.status(404).json({ success: false, message: "Tour not Found" });
+        }
+        tour.isRejected = true;
+        tour.save();
+       return  res.status(200).json({ success: true, message: "Tour Approved Successfully" });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Failed to Accept" });
+    }
+}
